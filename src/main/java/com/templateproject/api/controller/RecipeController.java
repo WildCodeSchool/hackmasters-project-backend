32d01a6ce3ff4a44f8028994b445430851c38bae
TYLER DURNION <td665pro@.pm.me>
2023-07-05 11:56:20 +0200

    Get all recipes in controller

---
+++ src/main/java/com/templateproject/api/controller/RecipeController.java
@@ -0,0 +1,25 @@
+package com.templateproject.api.controller;
+
+import com.templateproject.api.repository.RecipeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recipes")
+public class RecipeController {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeController(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @GetMapping("")
+    public List<Recipe> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
+
+}
