7e89abe0eb6e4d9a66e697a0fac5a1c75f9798db
TYLER DURNION <td665pro@.pm.me>
2023-07-05 11:25:49 +0200

    Connection succesful, ready to create controllers

---
+++ src/main/java/com/templateproject/api/config/SecretsConfig.java
@@ -0,0 +1,9 @@
+package com.templateproject.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:secrets.properties")
+public class SecretsConfig {
+}
