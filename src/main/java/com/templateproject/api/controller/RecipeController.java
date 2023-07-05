9ba667f2c44fa22552ee47a22f1b821c50d2d070
TYLER DURNION <td665pro@.pm.me>
2023-07-05 14:44:16 +0200

    GET methods in place

--- src/main/java/com/templateproject/api/controller/RecipeController.java
+++ src/main/java/com/templateproject/api/controller/RecipeController.java
@@ -2,6 +2,7 @@ package com.templateproject.api.controller;
 
 import com.templateproject.api.repository.RecipeRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class RecipeController {
     @GetMapping("")
     public List<Recipe> getAllRecipes() {
         return recipeRepository.findAll();
+    }
+
+    @GetMapping("/{slug}")
+    public Recipe getRecipeById(@PathVariable String slug) {
+        return recipeRepository.findBySlug(slug).orElseThrow();
     }
 
 }
