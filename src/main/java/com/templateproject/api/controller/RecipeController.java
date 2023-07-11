6d2812f35420322cf6df0d874f038c994f836f24
Rémy Marquis <114175213+remy-rm@users.noreply.github.com>
2023-07-11 12:12:41 +0200

    add allergens

--- src/main/java/com/templateproject/api/controller/RecipeController.java
+++ src/main/java/com/templateproject/api/controller/RecipeController.java
@@ -2,12 +2,11 @@ package com.templateproject.api.controller;
 
 import com.templateproject.api.entity.Recipe;
 import com.templateproject.api.repository.RecipeRepository;
+import com.templateproject.api.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,11 +16,21 @@ import java.util.Optional;
 public class RecipeController {
     @Autowired
     RecipeRepository recipeRepository;
+    @Autowired
+    RecipeService recipeService;
 
     // Get all recipes
     @GetMapping
     public List<Recipe> getAllRecipes() {
         return recipeRepository.findAll();
+    }
+    @GetMapping("/allergens")
+    public ResponseEntity<List<Recipe>> getRecipesByAllergens(@RequestParam("allergen") List<String> allergenNames) {
+        List<Recipe> recipes = recipeService.getRecipesByAllergens(allergenNames);
+        if (recipes.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(recipes, HttpStatus.OK);
     }
 
 
--- src/main/java/com/templateproject/api/repository/RecipeRepository.java
+++ src/main/java/com/templateproject/api/repository/RecipeRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
+    List<Recipe> findByAllergensAllergenNameIn(List<String> allergenNames);
+
 }
